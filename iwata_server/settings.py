b3c3ee0a63fa1d412de4883cfcebe9411c530c5a
allan-js <codewithallan@gmail.com>
2024-12-03 13:19:14 +0300

    System updates for deployment

--- iwata_server/settings.py
+++ iwata_server/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Third Patry
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'iwata_server.urls'
